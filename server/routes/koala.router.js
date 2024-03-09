01840ef835b369af61cd0a08eda74b3fa2f191fc
David Smith <djsmith611@me.com>
2024-03-09 09:26:04 -0700

    created requests in router.

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,17 +2,26 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+let koalas = [];
 
 // GET
-
+koalaRouter.get('/', (req, res) => {
+    res.status(200).send(koalas);
+});
 
 // POST
-
+koalaRouter.post('/', (req, res) => {
+    koalas.push(req.body);
+    res.status(201);
+});
 
 // PUT
 
 
 // DELETE
+koalaRouter.delete('/', (req, res) => {
+    koalas.length = 0;
+    res.sendStatus(200);
+})
 
 module.exports = koalaRouter;
